0fb25462be2a5d29a2a8f20175706a74ec1486e4
standalonewebsocketserver <standalonewebsocketserver@9ab0f6b0-dbfa-e4ef-f26b-405dfe489fe8>
2011-01-11 19:40:11 +0000

    Updated with latest connectivity changes.
    New connections now handed off to connectionManager immediately, so apps are responsible for validation.

--- application.py
+++ application.py
@@ -1,6 +1,15 @@
 from connection import *
 import datetime
 import traceback
+import Queue
+
+#DB imports
+#import MySQLdb
+import sqlalchemy
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 class Application:
 
@@ -15,32 +24,56 @@ class Application:
         self.DeltaTime = 0
 
         self.ConnectionTimeout = None
-        
+        self.VerifyTimeout = None
+
+        self.PendingClientsQueue = Queue.Queue(0)        
         self.Clients = []
         self.CommandMap = {}
         #which connection the current command came from
         self.CommandConnectionContext = None
 
 
+    def AddPendingClient(self, connection):
+        #connection.SetTimeout(self.VerifyTimeout or self.ConnectionTimeout)
+        self.PendingClientsQueue.put(connection)
+
+    #Return:
+    #True = verified
+    #False = unverified or disconnected
+    #None = no credentials received yet
     def VerifyConnection(self, connection):
-        log.info("Application %s attempting to verify client..." % (self.Name))
-        connection.RecvCommands()
+        
         credentials = connection.GetNextCommand()
-        log.info("Credentials %s" % (credentials))
+        if not connection.Connected:
+            log.info("Application %s: Connection was dropped before it could be verified" % (self.Name))
+            return False
+        
+        elif credentials == None:
+            return None
+        
+        else:
+            #Accept any input as credentials
+            log.info("Credentials %s" % (credentials))
         
         return True
 
-    #Attempt to verify client. Return True if successful, False otherwise
+    #Attempt to verify client. Return True if successful,
+    #None if no credentials recevied yet,
+    #False otherwise
     def AddClient(self, connection):
-        log.info("Application %s attempting to add client..." % (self.Name))
+        
         verified = self.VerifyConnection(connection)
         if verified == True:
-            log.info("Connection was verified.")
+            log.info("Connection from [%s] was verified." % (repr(connection.ClientAddress)))
             self.Clients.append(connection)
             connection.SetTimeout(self.ConnectionTimeout)
             return True
+        
+        elif verified == None:
+            return None
+        
         else:
-            log.info("Connection could not be verified.")
+            log.info("Connection from [%s] could not be verified." % (repr(connection.ClientAddress)))
             return False
         
     def RemoveClient(self, connection):
@@ -53,28 +86,29 @@ class Application:
             log.info("Application %s dropped a client" % (self.Name))
             
         except ValueError:
-            log.info("Application %s tried to drop a client that it wasn't servicing" % (self.Name))
+            log.warning("Application %s tried to drop a client that it wasn't servicing" % (self.Name))
             pass
 
     #attempt to look up the handler for a command and return the result of running that logic
     def ProcessCommand(self, command):
-        log.info('Application %s processing a client command' % (self.Name))
+        log.debug('Application %s processing a client command' % (self.Name))
         parts = command.split('|')
 
         #log.info('  Command map for %s: %s' % (self.Name, repr(self.CommandMap)))
         if parts[0] in self.CommandMap:
             args = parts[1:]
             result = self.CommandMap[parts[0]](*args)
-            log.info('%s client Command %s returned %s' % (self.Name, command, result))
+            log.debug('%s client Command %s returned %s' % (self.Name, command, result))
             return result
         else:
-            log.info('%s got an Unknown command %s' % (self.Name, command))
+            log.debug('%s got an Unknown command %s' % (self.Name, command))
             return None
 
     def Run(self, callback=None):
         while 1:
             try:
                 #main input processing for each client
+                client = None
                 for client in self.Clients:
                     self.CommandConnectionContext = None
                     
@@ -90,14 +124,38 @@ class Application:
                     else:
                         self.RemoveClient(client)
 
-                #call any custom callback logic
+                #################################
+                #Call any custom callback logic
+                #################################
                 if not callback == None:
                     callback()
-                        
+
+                #
+                #Verify and add any pending clients
+                #
+                pendingConnectionsToReAdd = []    
+                while not self.PendingClientsQueue.empty():
+                    pending = self.PendingClientsQueue.get()
+
+                    #If failed to verify client, or they disconnected, result will be False
+                    #If no credentials received yet, result will be None
+                    #Otherwise client passed verification and result will be True
+                    #Re-add pending connection to the queue only if no credentials were received yet and
+                    #they haven't timed out or disconnected
+                    addClientResult = self.AddClient(pending)
+                    if addClientResult == None:
+                        pendingConnectionsToReAdd.append(pending)
+                    
+                #Re-add pending connections that should be tested again later
+                for pending in pendingConnectionsToReAdd:
+                    self.AddPendingClient(pending)
+                    
             except Exception as ex:
                 self.CommandConnectionContext = None
-                self.RemoveClient(client)                
-                log.info('Application got an exception while servicing a client')
-                log.info(repr(ex))
+                if client != None:
+                    self.RemoveClient(client)
+                    
+                log.error('Application got an exception:')
+                log.error(repr(ex))
                 traceback.print_exc()
 
